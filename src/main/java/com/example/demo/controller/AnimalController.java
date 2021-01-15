08e05faddfcaf7fe52095010f2816160a7672036
Semih Celik <s.celikce@gmail.com>
2021-01-15 15:11:31 +0300

    Init project

---
+++ src/main/java/com/example/demo/controller/AnimalController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.Animal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+public class AnimalController {
+
+    private final AtomicInteger counter = new AtomicInteger();
+
+    @GetMapping("animals")
+    public Animal getAll() {
+        return getAnimal();
+    }
+
+    private Animal getAnimal() {
+        Animal animal = new Animal();
+        animal.setId(counter.incrementAndGet());
+        animal.setName("animal");
+
+        return animal;
+    }
+}
