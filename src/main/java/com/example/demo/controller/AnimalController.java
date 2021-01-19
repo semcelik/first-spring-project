f7a0dd52c7fdf123c9b10f80f540088cba84c36c
Semih Celik <s.celikce@gmail.com>
2021-01-19 11:13:43 +0300

    Update AnimalController.java

--- src/main/java/com/example/demo/controller/AnimalController.java
+++ src/main/java/com/example/demo/controller/AnimalController.java
@@ -19,7 +19,7 @@ public class AnimalController {
     private Animal getAnimal() {
         Animal animal = new Animal();
         animal.setId(counter.incrementAndGet());
-        animal.setName("animal-0.0.38");
+        animal.setName("animal-0.0.39");
 
         return animal;
     }
