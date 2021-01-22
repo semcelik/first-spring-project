c4045bbe366fa0235595bf33dc8f5ee4b85eff71
Semih Celik <s.celikce@gmail.com>
2021-01-22 12:37:26 +0300

    Update AnimalController.java

--- src/main/java/com/example/demo/controller/AnimalController.java
+++ src/main/java/com/example/demo/controller/AnimalController.java
@@ -14,7 +14,7 @@ public class AnimalController {
 
     @GetMapping("/")
     public String root() {
-        return "hello-amazon v0.0.79";
+        return "hello-amazon v0.0.80";
     }
 
     @GetMapping("animals")
@@ -25,7 +25,7 @@ public class AnimalController {
     private Animal getAnimal() {
         Animal animal = new Animal();
         animal.setId(counter.incrementAndGet());
-        animal.setName("animal-0.0.79");
+        animal.setName("animal-0.0.80");
 
         return animal;
     }
